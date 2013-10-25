9123a8d35d5f0dca38b3261130972579dd518117
Anna Armentrout <anna.armentrout@pellucid.com>
2013-10-25 18:01:03 -0400

    line graph of US GDP data

---
+++ public/js/productivity.js
@@ -0,0 +1,95 @@
+var data; // a global
+
+function visualizeit() {
+    //console.info("visualizing it");
+
+    /*
+    BAR CHART
+    var chart = d3.select("body").append("div").attr("class", "chart");
+
+    chart.selectAll("div")
+        .data(data.observations)
+        .enter().append("div")
+        .style("width", function(d) { return d.value / 100 + "px"; })
+        .text(function(d) { return d.date; });
+    */
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 100},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+    var parseDate = d3.time.format("%Y-%m-%d").parse;
+
+    var x = d3.time.scale()
+        .range([0, width]);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    var line = d3.svg.line()
+        .x(function(d) { return x(d.parsedate); })
+        .y(function(d) { 
+            if (isNaN(d.close))
+                return y(0);
+            else
+                return y(d.close); 
+        });
+
+    var svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    //console.info("requesting gdp");
+    d3.json("/gdp", function(error, json) {
+        if (error) return console.warn(error);
+        data = json;
+
+        //console.info("data: ", data);
+
+        data.observations.forEach(function(d) {
+            d.parsedate = parseDate(d.date);
+            d.close = +d.value;
+        });
+
+        x.domain(d3.extent(data.observations, function(d) { return d.parsedate; }));
+        y.domain(d3.extent(data.observations, function(d) { return d.close; }));
+
+        //console.info("line: ", line(data.observations));
+        svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+
+        svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+        .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("GDP ($)");
+
+        svg.append("path")
+          .datum(data.observations)
+          .attr("class", "line")
+          .attr("d", line);
+    });
+}
+
+visualizeit();
+
+
+
+
+
---
+++ public/js/server.js
@@ -0,0 +1,64 @@
+var sys = require("sys"),
+    static = require('node-static'),
+    http = require("http"),
+    url = require("url"),
+    filesys = require("fs"),
+    util = require("util");
+
+var webroot = './public',
+    port = 8080;
+
+var file = new(static.Server)(webroot, { 
+  cache: 600, 
+  headers: { 'X-Powered-By': 'node-static' } 
+});
+
+
+var indexHtml = filesys.readFileSync('./public/index.html');
+
+http.createServer(function(request, response) {
+    if (request.url === '/gdp') {
+        var self = response ;
+
+        var options = {
+            host: "api.stlouisfed.org",
+            port: 80,
+            path: "/fred/series/observations?series_id=GDP&api_key=8fb0d05280c678807993e6ef5e7e95a4&file_type=json",
+            method: "GET"
+        }
+        
+        var callback = function(response) {
+            console.log("gdp request status: " + response.statusCode);
+            var str = '';
+
+            response.on('data', function(chunk) {
+                str += chunk;
+            });
+            
+            response.on('end', function() {
+                console.info("got the data!");
+                self.writeHeader(200, {"Content-Type": "application/json"});
+                self.write(str);
+                self.end();
+            });
+        }
+        
+        http.request(options, callback).end();
+    } else {
+        file.serve(request, response, function(err, result) {
+          if (err) {
+            console.error('Error serving %s - %s', request.url, err.message);
+            if (err.status === 404 || err.status === 500) {
+              file.serveFile(util.format('/%d.html', err.status), err.status, {}, request, response);
+            } else {
+              response.writeHead(err.status, err.headers);
+              response.end();
+            }
+          } else {
+            console.log('%s - %s', request.url, response.message); 
+          }
+        });
+    }
+}).listen(port);
+sys.puts("Server Running on 8080");
+
